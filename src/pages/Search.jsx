46bfbc1bc9c82b94ab3dae78b8f69a29df6f990e
Joao Keuwe <joaov2872@hotmail.com>
2022-01-15 22:47:29 -0300

    requisito 05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,57 @@
 import React from 'react';
 import Header from '../components/Header';
 
+const DOIS = 2;
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      buttonDisabled: false,
+      inputMusic: '',
+    };
+  }
+
+  handleMusic = (event) => {
+    const { target: { value, id } } = event;
+    this.setState({ [id]: value });
+    this.handlevalidate();
+  }
+
+  handlevalidate = () => {
+    const { inputMusic } = this.state;
+    if (inputMusic.length >= DOIS) {
+      return this.setState({ buttonDisabled: true });
+    } return this.setState({ buttonDisabled: false });
+  }
+
   render() {
+    const { buttonDisabled, inputMusic } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form action="">
+          <label htmlFor="inputMusic">
+            <input
+              type="text"
+              value={ inputMusic }
+              data-testid="search-artist-input"
+              placeholder="digite o pancadão"
+              id="inputMusic"
+              onChange={ this.handleMusic }
+            />
+          </label>
+
+          <button
+            id="buttonDisabled"
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ !buttonDisabled }
+          >
+            Pesquisar
+            {' '}
+
+          </button>
+        </form>
       </div>
     );
   }
