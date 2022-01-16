ab72ba087433fa0f431621574f1bd6cf37bd840a
Joao Keuwe <joaov2872@hotmail.com>
2022-01-15 23:07:45 -0300

    requisito 05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,42 +2,45 @@ import React from 'react';
 import Header from '../components/Header';
 
 const DOIS = 2;
+// definindo estados atuais
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
-      buttonDisabled: false,
       inputMusic: '',
     };
   }
 
+  // capturando eventos que forem passados para o input pela func handlemusic
   handleMusic = (event) => {
     const { target: { value, id } } = event;
     this.setState({ [id]: value });
-    this.handlevalidate();
   }
 
-  handlevalidate = () => {
+  // condição para que o botão seja desabilitado no inicio caso não haja  ao menos dois Caracteres
+  buttonDisabled= () => {
     const { inputMusic } = this.state;
-    if (inputMusic.length >= DOIS) {
-      return this.setState({ buttonDisabled: true });
-    } return this.setState({ buttonDisabled: false });
+    if (inputMusic.length < DOIS) {
+      return false;
+    } return true;
   }
 
   render() {
-    const { buttonDisabled, inputMusic } = this.state;
+    const { inputMusic } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        {' '}
+        {/* renderizando o header */}
         <form action="">
           <label htmlFor="inputMusic">
             <input
               type="text"
-              value={ inputMusic }
+              value={ inputMusic } // passando a chave 'inputMusic' que está dentro do state
               data-testid="search-artist-input"
               placeholder="digite o pancadão"
               id="inputMusic"
-              onChange={ this.handleMusic }
+              onChange={ this.handleMusic } // chamando a função 'handleMusic' através do evento 'onChange'
             />
           </label>
 
@@ -45,7 +48,7 @@ class Search extends React.Component {
             id="buttonDisabled"
             type="submit"
             data-testid="search-artist-button"
-            disabled={ !buttonDisabled }
+            disabled={ !this.buttonDisabled() } // o '!' está alterando o valor incial da função 'buttonDisabled' para falso
           >
             Pesquisar
             {' '}
