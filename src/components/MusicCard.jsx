e3e543ef0ef5667361d1c251f021c4a42f6dd4eb
João Keuwe <joaov2872@hotmail.com>
2022-01-17 17:23:26 -0300

    req07

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class MusicCard extends React.Component{
-render() {
-  const {trackName,previewUrl }= this.props
-  return(
-    <section>
-      <h2>{trackName}</h2>
-      
+class MusicCard extends React.Component {
+  render() {
+    const { trackName, previewUrl } = this.props;
+    return (
+      <section>
+        <h2>{trackName}</h2>
+        <audio
+          data-testid="audio-component"
+          src={ previewUrl }
+          controls
+        >
+          <track kind="captions" />
+        </audio>
 
-  <audio
-      data-testid="audio-component"
-      src={ previewUrl } controls>
-      <track kind="captions" />
-  </audio>
-
-    </section>
-)}
+      </section>
+    );
+  }
 }
 
+MusicCard.propTypes = {
+  trackName: PropTypes.string,
+  previewUrl: PropTypes.string,
+}.isRequired;
 export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,47 +1,56 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import MusicCard from '../components/MusicCard';
-import {getMusics} from '../services/musicsAPI';
+import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
   constructor() {
-    super()
-  this.state ={
-    nameArtist: '',
-    arrayOfMusics: '',
-  }
-  }
-  componentDidMount(){ //usei componentDidMount para ser executado após o render().
-    this.handleMusics() 
-  }
-  handleMusics=() => {
-    const {match:{params:{id}}} =this.props //desenstruturando id dentro de params, e params dentro de match, e match dentro de props.
-    const responseMusic = await getMusics(id)// fazendo a requisição de 'getMusics' com o id
-    this.setState({arrayOfMusics: responseMusic})
+    super();
+    this.state = {
+      /* nameArtist: '', */
+      arrayOfMusics: '',
+    };
   }
 
-  
+  componentDidMount() { // usei componentDidMount para ser executado após o render().
+    this.handleMusics();
+  }
+
+  handleMusics= async () => {
+    const { match: { params: { id } } } = this.props; // desenstruturando id dentro de params, e params dentro de match, e match dentro de props.
+    const responseMusic = await getMusics(id);// fazendo a requisição de 'getMusics' com o id
+    this.setState({ arrayOfMusics: responseMusic });
+  }
+
   render() {
-    const {arrayOfMusics} = this.state;
+    const { arrayOfMusics } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        <p data-testid="artist-name">{arrayOfMusics[0].artistName}</p> {/* exibido nome do artista/album */}
+        <p data-testid="artist-name">{arrayOfMusics[0].artistName}</p>
+        {' '}
+        {/* exibido nome do artista/album */}
         <p data-testid="album-name">{arrayOfMusics[0].collectionName}</p>
-      {/*   <MusicCard /> */}
+        {/*   <MusicCard /> */}
 
-      {/* //filtrando para que  filter pegue somente os objetos que contém 'previewUrl' dentro do array 'arrayOfMusic */}
-      {arrayOfMusics.filter(({previewUrl}) => (previewUrl))
-        .map(({trackName, previewUrl})=>{ /* desconstruindo trackName e previweUrl */
-          {/* pasando um valor que não se repete */}
-          <section key={trackName}>
-            <MusicCard trackName={trackName} previewUrl={ previewUrl } />
-          </section>
-        })}
+        {/* //filtrando para que  filter pegue somente os objetos que contém 'previewUrl' dentro do array 'arrayOfMusic */}
+        {arrayOfMusics.filter(({ previewUrl }) => (previewUrl))
+          .map(({ trackName, previewUrl }) => (
+            <section key={ trackName }>
+              <MusicCard trackName={ trackName } previewUrl={ previewUrl } />
 
-      
+            </section>
+          ))}
       </div>
     );
   }
 }
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired; // validando as props, objeto e string
 export default Album;
